c0b8fb886f4a53d3ff71bab0e05e0bdab484944b
vgenty <vic.genty@gmail.com>
2013-10-02 11:09:29 -0500

    edited display.py to show pmt canvas

--- displays.py
+++ displays.py
@@ -3,23 +3,34 @@
 import grid
 import sys
 import pmt
+import styles as s
+from ROOT import TCanvas, TPaveText, TH2D, TH2I
+from ROOT import gStyle
 
 # './display.py filename.root events_number
 def main():
-    pp        = []
     file_name = sys.argv[1]
     event     = int(sys.argv[2])
     
-    #pp.append(Process(target=grid.gridit, args=(file_name,event)))
-    #pp.append(Process(target=pmt.pmt_draw, args=(file_name,event)))
-    
-    #for k in pp :
-    #    k.start()
-    #for j in pp :
-    #    k.join()
-    
     grid.gridit(file_name,event)
-    pmt.pmt_draw(file_name,event)
 
+    
+    #Draw PMT view
+    s.style()
+    pmt_canvas=TCanvas("pmt_canvas","pmt_canvas")
+    pmt_canvas.cd()
+    pmt_canvas.SetGridx()
+    pmt_canvas.SetGridy()
+    p = pmt.pmt_draw(file_name,event)
+    p[0].Draw("COLZ")
+    for y in p[1]:
+        y.Draw("SAMES")
+    p[2].Draw("SAMES")
+    pmt_canvas.Update()
+    pmt_canvas.Modified()
+    
+    sys.stdin.readline()
+    
 if __name__ == '__main__':
     main()
+
--- pmt.py
+++ pmt.py
@@ -1,15 +1,14 @@
 import styles      as s
 import fibers      as f
 import pmt_methods as m
-import sys
-from ROOT import TCanvas, TH2D, gStyle, TFile
+from ROOT import TH2D, TFile
 from ROOT import TPaveText  
 
 
 def pmt_draw(the_file,event):
-    s.style()    
-    c=TCanvas("c1","c1");
+    #Map PMT - 4X4 grid
     face=m.pmtface()
+    
     #Create Text
     text = [m.tpaver("%s" % str(x+4*y),float((x+1)%16),float((y+1)%16)) for x in xrange(4) for y in xrange(4)]    
     title=m.pmttitle("PMT View")
@@ -18,20 +17,13 @@ def pmt_draw(the_file,event):
     values = f.fiber(the_file,event)
     pmt = m.pmt_hist(values,face)
 
-    #Fix plot and draw it on a grid
     s.fix_plot(pmt)
     pmt.SetTitle(";Column;Row")
-    pmt.Draw("COLZ")
-    c.SetGridy()
-    c.SetGridx()
-    c.Update()
-    c.Modified()
-
-    #Draw text
-    for y in text :
-        y.Draw("sames")
-    title.Draw("sames")
-    c.Update()
-    c.Modified()
     
-    sys.stdin.readline()
+    #Return
+    for_display=[]
+    for_display.append(pmt)
+    for_display.append(text)
+    for_display.append(title)
+
+    return for_display
