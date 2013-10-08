02b82c2435ad5734e2d1754f9acadb5e8b04c20b
Doug Davis <douglasdavis@utexas.edu>
2013-10-08 10:24:01 -0500

    gave titles to grid plot

--- displays.py
+++ displays.py
@@ -7,21 +7,35 @@ import styles as s
 from ROOT import TCanvas, TPaveText, TH2D, TH2I
 from ROOT import gStyle
 
+def fixtpt(tpt):
+    tpt.SetTextFont(63)
+    tpt.SetTextSize(22)
+    tpt.SetFillColor(0)
+    tpt.SetBorderSize(0)
+
 # './display.py filename.root events_number
 def main():
+
     file_name = sys.argv[1]
     event     = int(sys.argv[2])
     
+    #Draw 2D event display
     my_grid = grid.gridit(file_name,event)
     grid_can = TCanvas('grid_can','grid_can',650,1100)
     grid_can.Divide(1,2)
     grid_can.cd(1)
     my_grid[0].Draw('colz')
-    grid_can.Update()
-    grid_can.Modified()
+    tpt1 = TPaveText(0.1287989,0.9150066,0.3688949,0.9585823,'brNDC')
+    tpt1.AddText('Top Grid')
+    fixtpt(tpt1)
+    tpt1.Draw('same')
     grid_can.cd(2)
     my_grid[1].Draw('colz')
-    
+    tpt2 = TPaveText(0.1287989,0.9150066,0.3688949,0.9585823,'brNDC')
+    tpt2.AddText('Bottom Grid')
+    fixtpt(tpt2)
+    tpt2.Draw('same')
+
     #Draw PMT view
     s.style()
     pmt_canvas=TCanvas("pmt_canvas","pmt_canvas")
